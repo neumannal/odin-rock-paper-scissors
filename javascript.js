b41a70f95b2a7e0f44df3a86d2fe9bd93bfbb1ce
Alexander Neumann <181498776+neumannal@users.noreply.github.com>
2024-11-20 22:12:17 +0100

    feat: add computer choice

--- javascript.js
+++ javascript.js
@@ -1,1 +1,18 @@
-console.log("hello");
+function getRandomInt(maxInt) {
+    // returns a random integer between zero and maxInt
+    return Math.floor(Math.random() * (maxInt + 1));
+}
+
+function getComputerChoice() {
+    // randomly return "rock", "paper" or "scissors"
+    let selection = getRandomInt(2);
+    if (selection === 0) {
+        return "rock";
+    } else if (selection === 1) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
+
+console.log(getComputerChoice());
